0e4070891f7aeeb4bb1b9d497909790332b9b09f
Sowmyaveerappan <sowmyabalamurugan1@gmail.com>
2024-10-13 22:03:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+var username = "Sowmya"
+var password = 123
+
+app.use(express.urlencoded({extended:true}))
+app.get("/login",function(req,res){
+
+    console.log(req.query.username)
+    if(req.query.username === username && req.query.password == password){
+        res.send(true)
+    }
+    else{
+        res.send(false)
+    }
+})
+
+app.listen(5000,function(){
+    console.log("Server started")
+})
